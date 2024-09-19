874ed366c9ddce3e86793315b7e7f735ca384705
Jaki <159941841+jaki-github@users.noreply.github.com>
2024-09-19 09:45:36 +0200

    Update s_airtag.lua

--- server/s_airtag.lua
+++ server/s_airtag.lua
@@ -3,6 +3,6 @@ QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateUseableItem("airtag", function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player then
-        TriggerClientEvent('qb-airtag:client:useAirtag', source)
+        TriggerClientEvent('jaki-airtag:client:useAirtag', source)
     end
 end)
